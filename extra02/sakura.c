807e9707896b408632614a7a3bb345132a070b46
Jerry Lee <lzjjerrywoodylee@gmail.com>
2016-05-21 18:20:21 +0900

    some update
    add js_beep
    change sakura.c to print wait msg

--- extra02/sakura.c
+++ extra02/sakura.c
@@ -2,12 +2,15 @@
 
 /* ハードコーディング　さくら */
 /* Jerry Lee */
-//#include <stdio.h>
+#include <stdio.h>
 #include <windows.h>
 int main(int argc, char *argv[]) {
-    //printf("Waiting for I/O Ready...\n");
+    printf("Waiting for I/O Ready...\n");
+    fflush(stdout);
     Sleep(600);
     Beep(0,600);
+    printf("Playing...\n");
+    fflush(stdout);
     Beep(440.000,600);
     Beep(440.000,600);
     Beep(493.883,1200);
---
+++ extra02/starwars.cpp
@@ -0,0 +1,43 @@
+/////////////////////////////////////////////////////////
+/// -- Created by C++                                 ///
+/// -- 03 November 2009                               ///
+/// -- [Star Wars Music w/ System Beeps challenge]    ///
+/////////////////////////////////////////////////////////
+#include <iostream>
+#include <windows.h>
+
+int main(void)
+{
+    HWND stealth;
+    AllocConsole();
+    stealth=FindWindowA("ConsoleWindowClass",NULL);
+    ShowWindow(stealth,0);
+   {
+   Sleep(300);
+{
+Beep(300,500);
+Sleep(50);
+Beep(300,500);
+Sleep(50);
+Beep(300,500);
+Sleep(50);
+
+Beep(250,500);
+Sleep(50);
+
+Beep(350,250);
+Beep(300,500);
+Sleep(50);
+
+Beep(250,500);
+Sleep(50);
+
+Beep(350,250);
+Beep(300,500);
+Sleep(50);
+
+getchar();
+return 0;
+}
+}
+}
